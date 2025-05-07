39f8f9d96c28a6be923e69b52c62e1bff8ffcebd
André Furlan <ensismoebius@gmail.com>
2025-05-07 04:11:09 -0300

    UI resizing and "hide" handle in editor

--- components/ExerciseInstructions.jsx
+++ components/ExerciseInstructions.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import { TextInput } from 'react-native';
+import { useRef, useState } from 'react';
+import { Animated, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function ExerciseInstructions({ styles, instruction })
 {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const heightAnim = useRef(new Animated.Value(styles.instructions.minHeight)).current;
+
+
+    function toggleInput()
+    {
+        Animated.timing(heightAnim, {
+            toValue: isVisible ? 0 : styles.instructions.minHeight,
+            duration: 100,
+            useNativeDriver: false,
+        }).start();
+
+        setIsVisible(!isVisible);
+    };
+
     return (
-        <TextInput
-            style={styles.instructions}
-            multiline
-            value={instruction}
-            editable={false}
-            autoCapitalize="none"
-            autoCorrect={false}
-            spellCheck={false}
-            accessibilityLabel="Instruções do exercício"
-            accessibilityRole="text"
-            accessibilityLiveRegion="polite"
-        />
+        <View>
+            <TouchableOpacity onPress={toggleInput} style={styles.horizontalHandleDimentions}>
+                <View style={styles.handle} />
+            </TouchableOpacity>
+            <Animated.View style={{ overflow: 'hidden', height: heightAnim }}>
+                <TextInput
+                    style={styles.instructions}
+                    multiline
+                    value={instruction}
+                    editable={false}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    spellCheck={false}
+                    accessibilityLabel="Instruções do exercício"
+                    accessibilityRole="text"
+                    accessibilityLiveRegion="polite"
+                />
+            </Animated.View>
+        </View >
     );
 }
